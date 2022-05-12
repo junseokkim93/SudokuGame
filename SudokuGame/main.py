8de605c3e82e3eaca56c62ed96d24568fd18edd6
Junseok Kim <junseok.kim@gmail.com>
2022-05-12 21:11:13 +0200

    main.py first commit trial

---
+++ SudokuGame/main.py
@@ -0,0 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("echo")
+args = parser.parse_args()
+print(args.echo)
+
+def main():
+    
+    pass
+    
+if __name__ == "__main__":
+    main()
